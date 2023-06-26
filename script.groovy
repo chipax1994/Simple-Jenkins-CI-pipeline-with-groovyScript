f1d8767fc9d878ba21d43888a50c21e1dbefb48d
PETER CHIMAOBI ONUOHA <54422829+chipax1994@users.noreply.github.com>
2023-06-26 13:27:12 +0100

    Add files via upload

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildApp() {
+    echo 'building the application...'
+} 
+
+def testApp() {
+    echo 'testing the application...'
+} 
+
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+
+
+
+return this 
